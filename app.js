0fdbaf1e6d7905a2e99c0972e364a83f8d79f503
fadysadakah.emh@gmail.com <fadysadakah.emh@gmail.com>
2019-05-05 18:25:21 +0200

    from

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 var i;
 var mailOptions;
-var transporter 
+var transporter
 
 // ***********************************************************************************************************
 function send(array) {
@@ -46,36 +46,42 @@ app.get('/', function (req, res) {
 })
 // ***********************************************************************************************************
 app.post('/', function (req, res) {
-console.log(req.body)
+    console.log(req.body)
 
     if (req.body.user.slice(-9) == 'yahoo.com') {
         transporter = nodemailer.createTransport({
-            service: 'yahoo', host: 'smtp.mail.yahoo.com', secure: true, port: 465, auth: {  user:req.body.user, pass: req.body.pass}
+            service: 'yahoo', host: 'smtp.mail.yahoo.com', secure: true, port: 465, auth: { user: req.body.user, pass: req.body.pass }
         });
-        
+
 
     } else if (req.body.user.slice(-9) == 'gmail.com') {
         transporter = nodemailer.createTransport({
-            service: 'gmail', host: 'smtp.gmail.com', secure: true, port: 465, auth: {  user: req.body.user, pass: req.body.pass}
+            service: 'gmail', host: 'smtp.gmail.com', secure: true, port: 465, auth: { user: req.body.user, pass: req.body.pass }
         });
-       
+
+    } else if (req.body.user.slice(-7) == 'aol.com') {
+        transporter = nodemailer.createTransport({
+            service: 'aol', host: 'smtp.aol.com', secure: false, port: 465, auth: { user: req.body.user, pass: req.body.pass }
+        });
     }
 
 
+
+
     mailOptions = {
-        from: req.body.user,
+        from: req.body.from+'<'+req.body.user+'>',
         to: '',
         subject: req.body.subject,
         text: req.body.text,
         html: req.body.html
     };
-    i = 0;
-    var array = JSON.parse(req.body.array)
-    console.log('\n***************************************************************************************************************\n')
-    console.log(transporter)
-    console.log('\n********************************************************************************************************************\n')
-    send(array);
-    res.render('index')
+i = 0;
+var array = JSON.parse(req.body.array)
+console.log('\n***************************************************************************************************************\n')
+console.log(transporter)
+console.log('\n********************************************************************************************************************\n')
+send(array);
+res.render('index')
 })
 // ***********************************************************************************************************
 app.listen(PORT, function () {
